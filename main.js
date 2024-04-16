a03b98721c18eb9660e70862dff503de4d594f31
Gabbo-CYBER <gabry29101998@gmail.com>
2024-04-16 17:36:06 +0200

    update

---
+++ main.js
@@ -0,0 +1,15 @@
+const id = prompt("Inserisci il tuo nome:");
+const nome = prompt("Inserisci il tuo cognome:") 
+const cognome = prompt("Inserire il colore")
+result = prompt(nome + cognome + colore);
+
+
+
+
+
+
+
+
+
+
+
